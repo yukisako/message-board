5de6d5bd0a81272482ce0c2747274b7e21b116c4
Yuki Sako <yukisakoatoz@gmail.com>
2015-09-08 13:03:27 +0900

    created Partial

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'messages#index'
   resources :messages, expect: [:index, :new]
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
