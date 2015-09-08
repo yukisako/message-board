06965d5d63b3fb53cef3722b875b7acae410508b
Yuki Sako <yukisakoatoz@gmail.com>
2015-09-08 18:10:23 +0900

    changed style

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -18,7 +18,7 @@ class MessagesController < ApplicationController
   end
 
   def create
-    binding.pry
+    #binding.pry
     @message = Message.new(message_params)
     if @message.save
       redirect_to root_path, notice: 'メッセージを保存しました'
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,8 @@
 class Message < ActiveRecord::Base
-  #名前は必須入力かつ20文字以内とする
-  validates :name, length: { maximum: 20 }, presence: true
-  #内容は必須入力かつ2文字以上30文字以内とする
-  validates :body, length:{ maximum:30, minimum:2}, presence:true
-  #年齢は任意入力、0歳以上、数字とする
-  validates :age, numericality: {only_integer:true, greater_than_or_equal_to: 0}, presence:false
+  #名前は必須入力かつ10文字以内とする
+  validates :name, length: { maximum: 10 }, presence: true
+  #内容は必須入力かつ2文字以上40文字以内とする
+  validates :body, length:{ maximum:40, minimum:2}, presence:true
+  #年齢は任意入力、0歳以上100歳以下、数字とする
+  validates :age, numericality: {only_integer:true, greater_than_or_equal_to: 0,less_than: 100}, presence:true
 end
