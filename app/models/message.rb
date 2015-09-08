3dcdae7c68f715e08866fb1921bc5cda96237746
Yuki Sako <yukisakoatoz@gmail.com>
2015-09-08 12:52:06 +0900

    Implementation of the validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,6 @@
 class Message < ActiveRecord::Base
+  #名前は必須入力かつ20文字以内とする
+  validates :name, length: { maximum: 20 }, presence: true
+  #内容は必須入力かつ2文字以上30文字以内とする
+  validates :body, length:{ maximum:30, minimum:2}, presence:true
 end
